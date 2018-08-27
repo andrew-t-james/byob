3b63824722f3fe914bf19379381fe10526826354
Andrew James <ajames20@gmail.com>
2018-08-27 15:18:25 -0600

    Update migrations schema to es6

--- db/migrations/20180827143047_initial.js
+++ db/migrations/20180827143047_initial.js
@@ -1,31 +1,27 @@
 
-exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable('users', function(table) {
-      table.increments('id').primary();
-      table.string('first_name');
-      table.string('last_name');
+exports.up = (knex, Promise) => Promise.all([
+  knex.schema.createTable('users', table => {
+    table.increments('id').primary();
+    table.string('first_name');
+    table.string('last_name');
 
-      table.timestamps(true, true);
-    }),
+    table.timestamps(true, true);
+  }),
 
-    knex.schema.createTable('saved_routes', function(table) {
-      table.increments('id').primary();
-      table.string('name');
-      table.string('start_location');
-      table.string('end_location');
-      table.integer('user_id').unsigned()
-      table.foreign('user_id')
-        .references('users.id');
+  knex.schema.createTable('saved_routes', table => {
+    table.increments('id').primary();
+    table.string('name');
+    table.string('start_location');
+    table.string('end_location');
+    table.integer('user_id').unsigned();
+    table.foreign('user_id').references('users.id');
 
-      table.timestamps(true, true);
-    })
-  ])
-};
+    table.timestamps(true, true);
+  })
+]);
 
-exports.down = function(knex, Promise) {
-  return Promise.all([
+exports.down = (knex, Promise) =>
+  Promise.all([
     knex.schema.dropTable('saved_routes'),
     knex.schema.dropTable('users')
-  ])
-};
+  ]);
