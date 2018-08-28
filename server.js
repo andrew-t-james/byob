d105637e114707119df27e5b32fd12f4228096ae
Quinlan Hill <quinhill@icloud.com>
2018-08-28 13:19:38 -0600

    Remove parser from eslint

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/api/v1/users', (request, response) => {
+  const users = database('users').select()
+  return response(200).json(users)
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
