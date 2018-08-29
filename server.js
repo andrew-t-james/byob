f048fd16eae308889a9a6c4a6e865708c07d6da1
Andrew James <ajames20@gmail.com>
2018-08-29 13:37:11 -0600

    Update naming conventions to match database schema

--- server.js
+++ server.js
@@ -13,34 +13,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/v1/users', (request, response) => {
-  const users = database('users').select()
-  return response(200).json(users)
-})
-
-
-
-
-
-
-
-app.get('/api/v1/favorites', (request, response) => {
-  database('saved_routes').select()
-  .then(favorites => response.status(200).json(favorites))
-  .catch(error => response.status(500).json({ error }));
+  const users = database('users').select();
+  return response(200).json(users);
 });
 
-app.get('/api/v1/:id/favorites', (request, response) => {
-  const { id } = request.params;
 
-  database('saved_routes').where('id', id).select()
-  .then(favorites => {
-    if(favorites.length) {
-      return response.status(200).json(favorites)
-    }
-    return response.status(404).json({error: '404: Resource not found'})
-  })
-  .catch(() => response.status(500).send({'Error':'500: Internal server error.'}))
-})
+
+
+
+
+app.get('/api/v1/saved_routes', (request, response) => {
+  database('saved_routes').select()
+    .then(savedRoutes => response.status(200).json(savedRoutes))
+    .catch(error => response.status(500).json({ error }));
+});
+
+app.get('/api/v1/saved_routes/:user_id', (request, response) => {
+  const { user_id } = request.params;
+
+  database('saved_routes').where('user_id', user_id).select()
+    .then(favorites => {
+      if (favorites.length) {
+        return response.status(200).json(favorites);
+      }
+      return response.status(404).json({error: '404: Resource not found'});
+    })
+    .catch(() => response.status(500).send({'Error':'500: Internal server error.'}));
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
