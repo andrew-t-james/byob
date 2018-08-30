17a60127acd2521d1eb54afc0944014ae8d319e4
Quinlan Hill <quinhill@icloud.com>
2018-08-30 09:07:42 -0600

    Write get, post, delete functions and tests

--- server.js
+++ server.js
@@ -12,19 +12,43 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.locals.users = [{"name": "Quin"}, {"name": "Hill"}, {"name": "Andrew"}];
-
 app.get('/api/v1/users', (request, response) => {
-  const users = app.locals.users;
-  return response.status(200).json(users);
+  database('users').select()
+    .then((users) => (
+      response.status(200).json(users)
+    ))
+    .catch((error) => (
+      response.status(500).json({error})
+    ));
 });
 
 app.post('/api/v1/users', (request, response) => {
   const user = request.body;
-  app.locals.users.push(user);
-  return response.status(201).json(user);
+
+  for (let requiredParameters of [
+    'first_name',
+    'last_name'
+  ]) {
+    if (!user[requiredParameters]) {
+      return response
+        .status(422)
+        .send({error: `Expected format: { name: <String> }. 
+        You're missing a "${requiredParameters}" property.`});
+    }
+  }
+  database('users').insert(user, 'id')
+    .then(user => (
+      response.status(201).json({ id: user[0] })
+    ))
+    .catch(error => (
+      response.status(500).json({ error })
+    ));
 });
 
+app.delete('/api/v1/users', (request, response) => {
+  database('users').where('id', request.params.id).del()
+    .then(user => user);
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
--- test/server.spec.js
+++ test/server.spec.js
@@ -19,3 +19,37 @@ describe('Initial Test setup', () => {
         throw error;
       }));
 });
+      
+describe('GET /api/v1/users', () => {
+    
+  beforeEach(done => {
+    knex.migrate.rollback()
+      .then(() => {
+        knex.migrate.latest()
+          .then(() => {
+            return knex.seed.run()
+              .then(() => {
+                done();
+              });
+          });
+      });
+  });
+    
+  it('should return all users', done => {
+    chai.request(server)
+      .get('/api/v1/users')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.equal(30);
+        res.body[0].should.have.property('first_name');
+        res.body[0].first_name.should.equal('Jeff');
+        res.body[0].should.have.property('last_name');
+        res.body[0].last_name.should.equal('Root');
+        res.body[0].should.have.property('id');
+        res.body[0].id.should.equal(1);
+        done();
+      });
+  });
+});
